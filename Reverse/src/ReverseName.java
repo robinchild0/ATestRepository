df11c4da3af7087464b460664ed4d5f0acc94bc2
Francis Adams <AdamsFA19@pwcs-edu.org>
2019-01-02 14:01:55 -0500

    reverse

---
+++ Reverse/src/ReverseName.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ReverseName {
+    
+    public ReverseName()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scan.nextLine();
+        String backwards = "";
+        for (int i = name.length() - 1; i >= 0; i -=2) // i am grabbing every character
+        {
+            backwards += name.charAt(i);
+        }
+        scan.close();
+        System.out.println(backwards);
+    }
+    
+    public static void main(String[] args) {
+        new ReverseName();
+    }
+
+}
