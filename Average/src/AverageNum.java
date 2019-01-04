9687ea4f73af63f70aea83abe107cb2d8dade657
Francis Adams <AdamsFA19@pwcs-edu.org>
2019-01-04 13:01:53 -0500

    Fixed Kyle's problem

--- Average/src/AverageNum.java
+++ Average/src/AverageNum.java
@@ -9,7 +9,7 @@ public class AverageNum
 		System.out.println("Please type 2 numbers on two new lines");
 		int num1 = Integer.parseInt(scan.nextLine());
 		int num2 = Integer.parseInt(scan.nextLine());
-		System.out.println(num1/num2);//This is how you do an average right? Right?
+		System.out.println((num1 + num2) / 2.0);//This is how you do an average. Duh
 	}
 
 }
